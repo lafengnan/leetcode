0f563ef1f2b8e5ec72875c7f05ee4544f16464cf
Chris <panzb@allinmoney.com>
2016-09-12 12:59:02 +0800

    Add comments to twoSum solution.

--- src/array/ArraySolution.java
+++ src/array/ArraySolution.java
@@ -86,6 +86,17 @@ public class ArraySolution {
         return Search.biSearch(nums, 0, nums.length - 1, target);
     }
 
+    /**
+     * Given an array of integers, return indices of the two numbers such that they add up
+     * to a specific target. You may assume that each input would have exactly one solution.
+     * Example:
+     * Given nums = [2, 7, 11, 15], target = 9,
+     * Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+     * @param nums The input data
+     * @param target The target data for analysis
+     * @return The partial data index of target
+     * @throws Exception
+     */
     public int[] twoSum(int[] nums, int target) throws Exception {
 
         Map<Integer, Integer> map = new HashMap<>(nums.length);
